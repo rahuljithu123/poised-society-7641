74bdcbb763e12b535e888fc5b0d1672a4b6c0997
RahulGeorge <rahulrsngs@gmail.com>
2022-10-02 17:02:34 +0530

    added-new1

--- poised-society-7641/src/Components/Home/Main.jsx
+++ poised-society-7641/src/Components/Home/Main.jsx
@@ -1,6 +1,7 @@
 import {
   Container,
   Box,
+  Image,
   Tabs,
   TabList,
   TabPanels,
@@ -26,10 +27,17 @@ import {
   HStack,
   Spinner,
   Heading,
+  Icon,
+  Checkbox
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BoxShadow, hoverColor } from "../Variables";
+import { BiEditAlt } from "react-icons/bi";
+import { AiFillDollarCircle,AiFillMessage } from "react-icons/ai";
+// import SimpleBarReact from "simplebar-react";
+//  import "simplebar/src/simplebar.css";
+
 
 function Main() {
   const [isLoading, setIsLoading] = useState(true);
@@ -92,10 +100,12 @@ function Main() {
     return (
       <>
         <Flex
-          flexWrap="wrap"
+        display="flex"
+          
           justify="space-between"
           gap="2"
-          w={isLargerThan768 ? "50%" : "90%"}
+          mb="20px"
+          // w={isLargerThan768 ? "50%" : "90%"}
         >
           <InputGroup>
             <InputLeftAddon children={isLargerThan492 ? "Going to" : "GT"} />
@@ -235,6 +245,10 @@ function Main() {
             </Popover>
           </InputGroup>
         </Flex>
+        <Box ml="-800px">
+        <Checkbox ml="20px" >Add a flight</Checkbox>
+        <Checkbox  ml="20px">Add a car</Checkbox>
+        </Box>
         <Button
           colorScheme="blue"
           onClick={(e) => {
@@ -252,20 +266,23 @@ function Main() {
     return (
       <>
         <Flex
-          flexWrap="wrap"
+        display="flex"
+          // flexWrap="wrap"
           justify="space-between"
           gap="2"
           w={isLargerThan768 ? "50%" : "90%"}
+          mb="20px"
         >
-          <InputGroup>
+          
+          <InputGroup ml="-300px">
             <InputLeftAddon
               children={isLargerThan492 ? "Leaving from" : "LF"}
             />
-            <Input placeholder="Enter a location" />
+            <Input width="200px"  placeholder="Enter a location" />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon children={isLargerThan492 ? "Going to" : "GT"} />
-            <Input placeholder="Enter a location" />
+            <Input width="200px" placeholder="Enter a location" />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon children={isLargerThan492 ? "Check-in" : "CI"} />
@@ -445,13 +462,12 @@ function Main() {
             >
               <Box>
                 <Heading as="h1" color="white">
-                  Save instantly with Expedia Rewards
+                Members can save on holiday trips
                 </Heading>
               </Box>
               <Box>
                 <Text color="white" fontSize="xl">
-                  You can enjoy access to perks like Member Prices, saving you
-                  10% or more on select hotels. Terms may apply.
+                Make that family reunion finally happen this year with member-only discounts on select hotels
                 </Text>
               </Box>
               <Box>
@@ -463,29 +479,88 @@ function Main() {
                     colorScheme="blue"
                     size="lg"
                   >
-                    See Member Prices
+                    Find holiday savings
                   </Button>
                 </Link>
               </Box>
             </Flex>
           </Container>
          
-         <Container  mt="20px">
-          <Text display="flex" justifyContent="center" fontSize={25}>Here to help keep you on the move</Text>
+         <Container   maxW="container.xl"  mt="20px">
+          <Text display="flex" justifyContent="center" fontSize="30px" fontWeight="bold">Here to help keep you on the move</Text>
           <Flex>
-          <Box  border="1px" borderColor="black.200" borderRadius="10px" w="5000px" mt='20px' ml="50px" p='6'  h="320px" rounded='md' bg='white'>
-            <Text fontWeight="bold">Change or cancel a trip</Text>
+          <Box  border="1px" borderColor="black.200" borderRadius="10px" w="5000px" mt='20px' ml="50px" p='6'  h="180px" rounded='md' bg='white'>
+            <Box display="flex" justifyContent="space-between"> <Text fontWeight="bold" fontSize="22px">Change or cancel a trip</Text>
+            <Icon as={BiEditAlt} w={3.5} h={3.5} /></Box>
             <Text>Make updates to your itinerary or cancel a booking</Text></Box>
-          <Box border="1px" borderColor="black.200" borderRadius="10px"  w="5000px" mt='20px' ml ="50px"  h="320px" rounded='md' bg='white'><Text fontWeight="bold">Use a credit or coupon</Text>
+          <Box border="1px" borderColor="black.200" borderRadius="10px"  w="5000px" mt='20px' ml ="50px"  h="180px" rounded='md' bg='white'>
+            <Box display="flex" justifyContent="space-between">  
+            <Text fontWeight="bold" fontSize="22px" mt="20px">Use a credit or coupon</Text>
+            <Icon as={AiFillDollarCircle} w={3.5} h={3.5} />
+            </Box>
+            
           <Text>Apply a coupon code or credit to a new trip</Text></Box>
-          <Box  border="1px" borderColor="black.200" borderRadius="10px"  mt='20px' ml="50px" p='6' w="5000px" h="320px" rounded='md' bg='white'>
-            <Text fontWeight="bold">Track your refund</Text>
-            <Text>Check the status of  a refund currently in progress</Text></Box>
+          <Box  border="1px" borderColor="black.200" borderRadius="10px"  mt='20px' ml="50px" p='6' w="5000px" h="180px" rounded='md' bg='white'>
+             <Box display="flex" justifyContent="space-between"> <Text fontSize="22px" fontWeight="bold">Track your refund</Text>
+             <Icon as={AiFillMessage} w={3.5} h={3.5} /></Box>
+             <Text>Check the status of  a refund currently in progress</Text>
+          
+             </Box>
           </Flex>
        
          </Container>
+        <Container  display="flex" justifyContent="center" maxW="container.xl" mt="20px" >
+          <Box ml="50px" >      <Image border="1px" borderRadius="10px" w="300px" height="300px" src='https://a.travel-assets.com/travel-assets-manager/ceorg-1514/HP-Edi-3UP-384x256px.jpg' alt='Dan Abramov' />
+           <Text>GET MORE TO GO MORE</Text>
+           <Text fontWeight="bold">Members can double dip with points on top of airline miles</Text>
+          </Box>
+          <Box ml="50px"><Image border="1px"  borderRadius="10px" w="300px" height="300px" src='https://a.travel-assets.com/travel-assets-manager/cmct-5183/US-CA-MX-HP-Editorial-3up-384x256.jpg' alt='Dan Abramov' />
+           <Text>BROADEN YOUR HORIZONS</Text>
+           <Text fontWeight="bold">There's a whole lot of world out there—go see more of it</Text></Box>
+          <Box ml="50px"><Image border="1px"  borderRadius="10px" w="300px" height="300px" src='https://a.travel-assets.com/travel-assets-manager/cmct-4898/HP-Editorial-3up-384x256.jpg' alt='Dan Abramov' />
+           <Text>YOUR WEEKEND JUST GOT BETTER</Text>
+           <Text fontWeight="bold">Save on these last-minute weekend getaways.</Text></Box>
+        </Container>
+         <Container display="flex" justifyContent="center" maxW="container.xl" mt="20px">
+    <Box ml="50px"><Image border="1px"  borderRadius="10px" w="600px" height="300px" src='https://a.travel-assets.com/travel-assets-manager/ceorg-1512/POSa-HP-Edi-2UP-457x257.jpg' alt='Dan Abramov' />
+           <Text>SEE MEMBER PRICES ON FALL TRIPS</Text>
+           <Text fontWeight="bold">Experience the joy of the season with an autumn getaway</Text></Box>
+    <Box ml="50px"> <Image border="1px"  borderRadius="10px" w="600px" height="300px" src='https://a.travel-assets.com/travel-assets-manager/4943-madetotravel/MTT-HP-Editorial-2up-457x257-5.jpg' alt='Dan Abramov' />
+           <Text>CITY VIEWS</Text>
+           <Text fontWeight="bold">So much to see, so much to do</Text></Box>
+         </Container>
+         <Container maxW="container.xl" border="1px"
+         mt="20px"
+            borderColor="black.200"
+            borderRadius="10px"  width="5000px" height="320px" display="flex">
+              <Box> <Image border="1px" ml="-20px"  borderRadius="10px" w="600px" height="320px" src='https://a.travel-assets.com/mad-service/footer/bnaBanners/BEX_ROME_iStock_72dpi.jpg' alt='Dan Abramov'/></Box>
+               <Box><Box display="flex"><Text fontWeight="bold" fontSize="30px" mr="400px">Our app takes you further</Text>
+               <Box><Image  width="90px" height="90px" src="https://a.travel-assets.com/mad-service/qr-code/footer_qr_hp/1_QR_FOOTER_HP.png"/>
+               <Text fontSize="15px" fontWeight="bold">Scan the QR code</Text></Box></Box>
+               <Text fontSize="14px" >When you book on the app you can save even more — up to 25% on select hotels while earning double the points with every booking.
+                With these app deals you'll save even more on trips, and that means you can take more trips, and manage it all on the go</Text>
+                <Text fontWeight="bold" fontSize="20px" mb="20px">Text yourself a download link for easy access</Text>
+                <Input
+ placeholder="Country code"
+ borderColor="black.200"
+ size="md"
+ ml="20px"
+ type="country" width="220px" mb="20px"/>
 
-         
+<Input
+ placeholder="Phone number"
+ borderColor="black.200"
+ size="md"
+ ml="20px"
+ type="country" width="220px"/>
+
+<Button ml="20px" colorScheme='blue'>Get The App</Button>
+<Text fontSize="14px">By providing your number, you agree to receive a one-time automated text message with a link to get the app. Standard text message rates may apply.</Text>
+    </Box>
+         </Container>
+
+<Container> </Container>
+        
         </>
       )}
     </Container>
